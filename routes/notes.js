0305bd39b85549f5f6b2b2056fa0ff5e1781c5f7
NeelCheo <73446815+NeelCheo@users.noreply.github.com>
2023-04-13 22:49:37 -0400

    made it so each note has a specific ID and also made th delete request work

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,6 @@
 const notes = require('express').Router();
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
+const unid = require('../helpers/unid');
 
 // GET Route for retrieving notes
 notes.get('/', (req, res) =>
@@ -14,6 +15,7 @@ notes.post('/', (req, res) => {
     const newNote = {
       title,
       text,
+      id: unid(),
     };
 
     readAndAppend(newNote, './db/notes.json');
@@ -27,6 +29,35 @@ notes.post('/', (req, res) => {
   } else {
     res.json('Error in posting note');
   }
+});
+
+//DELETE route for deleting notes
+// DELETE route for deleting notes
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  const deleteNote = (noteId) => {
+    return readFromFile('./db/notes.json').then((data) => {
+      const noteData = JSON.parse(data);
+      const noteIndex = noteData.findIndex(note => note.id === noteId);
+      
+      if (noteIndex !== -1) {
+        noteData.splice(noteIndex, 1);
+        writeToFile('./db/notes.json', noteData);
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  deleteNote(noteId).then((response) => {
+    if (response) {
+      res.json('note deleted');
+    } else {
+      res.json('Error in finding note');
+    }
+  });
 });
 
 module.exports = notes;
